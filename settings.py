8535f0a7bd48be22dad464afa19696df81e98d12
tos_lubuntu <tosnufc@gmail.com>
2024-10-12 10:58:23 +0700

    add settings.py to gitignore

--- settings.py
+++
@@ -1,6 +0,0 @@
-working_dir = '/Videos/example'
-file_name = 'example.avi'
-result_folder = '/practice'
-chunk_lenght = 90 #seconds (#90 seconds works well with OpenAI whisper)
-encoding = 'cpu' # cpu, gpu, copy
-os = 'linux' # windows, mac, linux
