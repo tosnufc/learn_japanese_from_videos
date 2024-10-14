f99ee5dcfb2ae57dd09ddce3a8b1a21a675cc064
tos_lubuntu <tosnufc@gmail.com>
2024-10-14 09:40:20 +0700

    add settings.py back

---
+++ settings.py
@@ -0,0 +1,6 @@
+working_dir = '/study/Videos/nihongo101'
+file_name = 'nihongo101.avi'
+result_folder = '/practice'
+chunk_lenght = 90 #seconds (#90 seconds works well with OpenAI whisper)
+encoding = 'cpu' # cpu, gpu, copy
+os = 'linux' # windows, mac, linux
